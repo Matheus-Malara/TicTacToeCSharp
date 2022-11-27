b6995352d7c340d1bfd8449b8a1234fe18df73eb
rodrigo <rodrigo@gmxcheckout.com.br>
2022-11-26 23:29:19 -0300

    import inicial

---
+++ TorresTrabalho/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace TorresTrabalho
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.novoJogoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 48.75F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 51.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button7, 0, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(25, 50);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 61F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(235, 190);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 67);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(69, 58);
+            this.button4.TabIndex = 1;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 58);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(78, 67);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(72, 58);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(156, 67);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(74, 58);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(78, 131);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(72, 56);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(156, 131);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(74, 56);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(78, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 58);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(156, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 58);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(3, 131);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(69, 56);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novoJogoToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // novoJogoToolStripMenuItem
+            // 
+            this.novoJogoToolStripMenuItem.Name = "novoJogoToolStripMenuItem";
+            this.novoJogoToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.novoJogoToolStripMenuItem.Text = "Novo jogo";
+            this.novoJogoToolStripMenuItem.Click += new System.EventHandler(this.novoJogoToolStripMenuItem_Click);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Vez do: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem novoJogoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TorresTrabalho/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TorresTrabalho
+{
+    public partial class Form1 : Form
+    {
+        int[,] tabuleiro = new int[3, 3];
+        bool vezDoX;
+        public Form1()
+        {
+            InitializeComponent();
+            iniciaJogo();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void novoJogoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            iniciaJogo();
+        }
+        private void iniciaJogo()
+        {
+            button1.Text = "";
+            button1.Enabled = true;
+            button2.Text = "";
+            button2.Enabled = true;
+            button3.Text = "";
+            button3.Enabled = true;
+            button4.Text = "";
+            button4.Enabled = true;
+            button5.Text = "";
+            button5.Enabled = true;
+            button6.Text = "";
+            button6.Enabled = true;
+            button7.Text = "";
+            button7.Enabled = true;
+            button8.Text = "";
+            button8.Enabled = true;
+            button9.Text = "";
+            button9.Enabled = true;
+            vezDoX = true;
+            label2.Text = "X";
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    tabuleiro[i, j] = -1;
+                }
+            }
+        }
+
+        private void atualizaJogo(int i, int j, Button b)
+        {
+            int valor = vezDoX ? 1 : 0;
+            tabuleiro[i, j] = valor;
+            string valorBotao = vezDoX ? "X" : "O";
+            b.Text = valorBotao;
+            b.Enabled = false;
+            bool vencedor = verificaAcabou(valor);
+            if (vencedor)
+            {
+                MessageBox.Show("O Jogador " + valorBotao + " VENCEU!");
+                iniciaJogo();
+                return;
+            }
+            bool empate = verificaEmpate();
+            if (empate)
+            {
+                MessageBox.Show("Deu VELHA, nenhum jogador ganhou.");
+                iniciaJogo();
+                return;
+            }
+            vezDoX = !vezDoX;
+            valorBotao = vezDoX ? "X" : "O";
+            label2.Text = valorBotao;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            atualizaJogo(0, 0, button1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            atualizaJogo(0, 1, button2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            atualizaJogo(0, 2, button3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            atualizaJogo(1, 0, button4);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            atualizaJogo(1, 1, button5);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            atualizaJogo(1, 2, button6);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            atualizaJogo(2, 0, button7);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            atualizaJogo(2, 1, button8);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            atualizaJogo(2, 2, button9);
+        }
+        private bool verificaAcabou(int jogador)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (tabuleiro[i,0] == tabuleiro[i,1] && 
+                    tabuleiro[i, 0] == tabuleiro[i, 2] &&
+                    tabuleiro[i,0] == jogador)
+                {
+                    return true;
+                }
+            }
+            for (int j = 0; j < 3; j++)
+            {
+                if (tabuleiro[0, j] == tabuleiro[1, j] &&
+                    tabuleiro[0, j] == tabuleiro[2, j] &&
+                    tabuleiro[0, j] == jogador)
+                {
+                    return true;
+                }
+            }
+            if (tabuleiro[0, 0] == tabuleiro[1, 1] &&
+                    tabuleiro[0, 0] == tabuleiro[2, 2] &&
+                    tabuleiro[0, 0] == jogador)
+            {
+                return true;
+            }
+            if (tabuleiro[0, 2] == tabuleiro[1, 1] &&
+                    tabuleiro[0, 2] == tabuleiro[2, 0] &&
+                    tabuleiro[0, 2] == jogador)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool verificaEmpate()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (tabuleiro[i, j] == -1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
